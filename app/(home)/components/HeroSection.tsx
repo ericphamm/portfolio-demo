df9e808f4d0126d7fe6633ce4b30b157612d3da4
ericpham <phamwangthang@gmail.com>
2024-05-03 03:47:25 +0200

    HeroSection edited

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { MovingBorderBtn } from '@/components/ui/moving-border'
 import Link from 'next/link'
 import React from 'react'
+import Title from './Title'
 
 export default function HeroSection() {
   return (
@@ -8,18 +9,16 @@ export default function HeroSection() {
 		<div className='space-y-10 text-center lg:text-left'>
 			<h1 className='text-4xl lg:text-7xl font-bold'>
 				Nice to meet you! 
-				<br/> <span className='underline underline-offset-8 decoration-green-500'>{"Welcome"}</span>
+				{/* <br/> <span className='underline underline-offset-8 decoration-green-500'>{"Welcome"}</span> */}
 			</h1>
 			<p className='md:w-96 text-lg text-gray-300'>
 				{"I am a front end developer based in Prague."}
 			</p>
-			<Link href={"mailto:themail@gnail.com"} className=' inline-block group'>
-				<div>
-					<h1 className='text-3xl font-bold group-hover:text-green-400 transition-all '>Get in touch!</h1>
-					<div className='w-40 h-2 bg-green-500 rounded-full'></div>
-					<div className='w-40 h-2 bg-indigo-500 rounded-full translate-x-2'></div>
-				</div>
-			</Link>
+			<div className='bottom-5 sm:bottom-14 left-0 sm:left-10 '>
+				<MovingBorderBtn href={"mailto:themail@gnail.com"} borderRadius='0.5rem' className='p-3 font-semibold'>
+					<p>Available for work</p>
+				</MovingBorderBtn>
+			</div>
 		</div>
 		<div className='relative'>
 			<div className='w-72 h-72 space-y-3 -rotate-[30deg]'>
@@ -32,11 +31,7 @@ export default function HeroSection() {
 					<div className='w-32 h-32 rounded-full bg-green-500'></div>
 				</div>
 			</div>
-			<div className='absolute bottom-5 sm:bottom-14 left-0 sm:left-10 '>
-				<MovingBorderBtn borderRadius='0.5rem' className='p-3 font-semibold'>
-					<p>Available for work</p>
-				</MovingBorderBtn>
-			</div>
+
 		</div>
 	</div>
   )
---
+++ app/(home)/components/Skills.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from './Title'
+
+export default function Skills() {
+  return (
+	<div>
+		<Title text="Skills" className='flex flex-col items-center justify-center'/>
+	</div>
+  )
+}
