1eeaad644d52517a7626082dee260866579894f7
ericpham <phamwangthang@gmail.com>
2024-05-05 03:28:09 +0200

    New HeroSection photo

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -8,8 +8,8 @@ const words = `Oxygen gets you high. In a cata You accept your fate.  Blank face
 
 export default function HeroSection() {
   return (
-	<div className='min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between animate-move-up'>
-		<div className='space-y-10 text-center lg:text-left'>
+	<div className='min-h-[60vh] flex flex-col-reverse gap-10 lg:gap-0 lg:flex-row items-center justify-between animate-move-up  '>
+		<div className='space-y-10 text-center lg:text-left '>
 			<h1 className='text-4xl lg:text-7xl font-bold'>
 				Nice to meet you! 
 				{/* <br/> <span className='underline underline-offset-8 decoration-green-500'>{"Welcome"}</span> */}
@@ -24,16 +24,9 @@ export default function HeroSection() {
 				</MovingBorderBtn>
 			</div>
 		</div>
-		<div className='relative'>
-			<div className='w-72 h-72 space-y-3 -rotate-[30deg]'>
-				<div className='flex gap-3 translate-x-8'>
-					<div className='w-32 h-32 rounded-2xl bg-green-500'></div>
-					<div className='w-32 h-32 rounded-full bg-indigo-500'></div>
-				</div>
-				<div className='flex gap-3 -translate-x-8'>
-					<div className='w-32 h-32 rounded-2xl bg-indigo-500'></div>
-					<div className='w-32 h-32 rounded-full bg-green-500'></div>
-				</div>
+		<div className=''>
+			<div className='w-96 h-96'>
+				<img src="heroimg.png" alt="" />
 			</div>
 
 		</div>
